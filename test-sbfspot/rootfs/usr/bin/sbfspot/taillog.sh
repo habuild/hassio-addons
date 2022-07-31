d3e9ad561fbe7189ca03483a89f514fe2921b924
habuild <36288425+habuild@users.noreply.github.com>
2022-07-31 20:59:42 +1000

    Update taillog.sh

--- test-sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
+++ test-sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
@@ -2,17 +2,23 @@
 
 cd /
 
-#create lognow variable
+#---- Create lognow variable
 lognow=$(date +%Y%m%d)
 
 # Sleep until log updated from last SBFspot run
 sleep 45
 
-# set tail to log out to HA log
-# Use lognow variable to select latest file
-echo '[SBFspot Upload Log Latest]'
-tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log >> /proc/1/fd/1
+#---- set tail to log out to HA log
+#---- Use lognow variable to select latest file
+#---- Print tail of latest log
 
+message=$(echo "[SBFspot Upload Log Latest] $(tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log)")
+
+## Print the message the user supplied, defaults to "Hello World..."
+bashio::log.info "${message:="Hello World..."}"
+
+
+#---- Delete old log files 
 #busybox command for minus 1 day
 #date +%Y%m%d -d @$(($(date +%s)-86400))
 
