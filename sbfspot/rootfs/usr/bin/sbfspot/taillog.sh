551516b6b8ae91f741f6cfbd5fbf1cf53492fda8
habuild <36288425+habuild@users.noreply.github.com>
2022-08-13 13:52:38 +1000

    Update taillog.sh

--- sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
+++ sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-#---- old method
-# set tail to log out to HA log
-# Use lognow variable to select latest file
-# echo '[SBFspot Upload Log Latest]'
-# tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log >> /proc/1/fd/1
-
-
 cd /
 
 #---- Create lognow variable
@@ -19,10 +12,11 @@ sleep 45
 #---- Use lognow variable to select latest file
 #---- Print tail of latest log
 
-message=$(echo "[SBFspot Upload Log Latest] $(tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log)")
-
+#message=$(echo "[SBFspot Upload Log Latest] $(tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log)")
+#bashio::log.info "${message:="Hello World..."}"
 ## Print the message the user supplied, defaults to "Hello World..."
-bashio::log.info "${message:="Hello World..."}"
+
+bashio::log.info "[SBFspot Upload Log Latest] $(tail -n1 "/data/sbfspot/log/SBFspotUpload$lognow.log")"
 
 
 #---- Delete old log files 
@@ -31,4 +25,3 @@ bashio::log.info "${message:="Hello World..."}"
 
 # remove log files older than 3 days
 find /data/sbfspot/log -mtime +3 -exec rm {} \;
-
