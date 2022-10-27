55e029ff5846a8f83a4538b00a388c55aa8fdc90
habuild <36288425+habuild@users.noreply.github.com>
2022-10-28 09:57:09 +1100

    Update taillog.sh

--- sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
+++ sbfspot/rootfs/usr/bin/sbfspot/taillog.sh
@@ -3,25 +3,25 @@
 cd /
 
 #---- Create lognow variable
-lognow=$(date +%Y%m%d)
-
-# Sleep until log updated from last SBFspot run
-sleep 45
+#  lognow=$(date +%Y%m%d)
 
 #---- set tail to log out to HA log
 #---- Use lognow variable to select latest file
 #---- Print tail of latest log
 
-#message=$(echo "[SBFspot Upload Log Latest] $(tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log)")
-#bashio::log.info "${message:="Hello World..."}"
+#   message=$(echo "[SBFspot Upload Log Latest] $(tail -n1 /data/sbfspot/log/SBFspotUpload$lognow.log)")
+#   bashio::log.info "${message:="Hello World..."}"
 ## Print the message the user supplied, defaults to "Hello World..."
 
-bashio::log.info "[SBFspot Upload Log Latest] $(tail -n1 "/data/sbfspot/log/SBFspotUpload$lognow.log")"
+# Sleep until log updated from last SBFspot run
+sleep 45
 
+bashio::log.info "[SBFspot Upload Log Latest]"     #$(tail -n1 "/data/sbfspot/log/SBFspotUpload$lognow.log")"
+# just used for timestamp display in logs.
 
 #---- Delete old log files 
 #busybox command for minus 1 day
 #date +%Y%m%d -d @$(($(date +%s)-86400))
 
-# remove log files older than 3 days
-find /data/sbfspot/log -mtime +3 -exec rm {} \;
+# remove log files older than 3 days no longer required, however keeping for the message in the log.
+##  find /data/sbfspot/log -mtime +3 -exec rm {} \; 
